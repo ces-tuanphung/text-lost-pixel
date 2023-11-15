a02d43cab2026537333df7f95f64116decea7070
ces-tuanphung <tuan.phung@codeenginestudio.com>
2023-11-15 16:16:42 +0700

    test lost pixel

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pageShots: {
+    pages: [{ path: "/", name: "landing" }],
+    baseUrl: "http://172.17.0.1:3000",
+  },
+  lostPixelProjectId: "clozjim4j7952g80eaq8aygrp",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
