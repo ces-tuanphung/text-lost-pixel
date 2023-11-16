b226c6cc8b4287de130176ee67df1332b6f5ae1e
ces-tuanphung <tuan.phung@codeenginestudio.com>
2023-11-16 11:54:16 +0700

    test delete

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   pageShots: {
-    pages: [{ path: "/lost", name: "landing" }, { path: "/", name: "lew lew"}],
+    pages: [
+      { path: "/lost", name: "landing" },
+      { path: "/", name: "test add" },
+      { path: "/case1", name: "test delete" },
+    ],
     baseUrl: "http://172.17.0.1:3000",
   },
   lostPixelProjectId: "clozjim4j7952g80eaq8aygrp",
